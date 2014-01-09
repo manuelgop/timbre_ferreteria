b0f340e7774d26fa48df4ce2bf530cfac309a7f5
Manuel <manuel@manuel-comal.(none)>
2014-01-09 12:51:54 -0600

    php file and script updated with 10 users

--- remote_realy.php
+++ remote_realy.php
@@ -8,21 +8,51 @@
 <body>
         
 <form id="relay" action="remote_relay.php" method="GET">
-    <input type="button" id="submitOn" style="border:1px solid #000; font-size:40px;" value="On" onClick="relayOn()"/>
-    <input type="button" id="submitOff" style="border:1px solid #000; font-size:40px;" value="Off" onClick="relayOff()"/>
+  <H1>Timbre</H1>
+  <br>
+    <input type="button" id="usuario1" style="border:1px solid #000; font-size:40px;" value="Usuario1" onClick="usuario1()"/>
+    <input type="button" id="usuario2" style="border:1px solid #000; font-size:40px;" value="Usuario2" onClick="usuario2()"/>
+    <input type="button" id="usuario3" style="border:1px solid #000; font-size:40px;" value="Usuario3" onClick="usuario3()"/>
+    <input type="button" id="usuario4" style="border:1px solid #000; font-size:40px;" value="Usuario4" onClick="usuario4()"/>
+    <input type="button" id="usuario5" style="border:1px solid #000; font-size:40px;" value="Usuario5" onClick="usuario5()"/>
+    <input type="button" id="usuario6" style="border:1px solid #000; font-size:40px;" value="Usuario6" onClick="usuario6()"/>
+    <input type="button" id="usuario7" style="border:1px solid #000; font-size:40px;" value="Usuario7" onClick="usuario7()"/>
+    <input type="button" id="usuario8" style="border:1px solid #000; font-size:40px;" value="Usuario8" onClick="usuario8()"/>
+    <input type="button" id="usuario9" style="border:1px solid #000; font-size:40px;" value="Usuario9" onClick="usuario9()"/>
+    <input type="button" id="usuario10" style="border:1px solid #000; font-size:40px;" value="Usuario10" onClick="usuario10()"/>
+    <input type="button" id="submitOff" style="border:1px solid #000; font-size:40px;" value="Apagar" onClick="relayOff()"/>
         <input type="hidden" name="total" id="total" value="">
 </form>
 
 <?php
-
+        $corto = 500000;
+        $largo = 1000000;
+        $extralargo = 1500000;
+        $espacio = 1000000;
         exec("gpio -g mode 4 out");
         $state = $_GET["total"];
-          
-        if ($state == "on"){
+          //Prende con 0 y apaga con 1
+        if ($state =="") {
+                exec("gpio -g write 4 1");
+        }
+        if ($state == "usuario1"){//Estado del usuario1
+                exec("gpio -g write 4 0");
+                usleep(corto);
+                exec("gpio -g write 4 1");
+
+        }
+        if ($state == "usuario2") {
+                exec("gpio -g write 4 0");
+                usleep(largo);
+                exec("gpio -g write 4 1");
+        }
+        if ($state == "usuario3") {
+                exec("gpio -g write 4 0");
+                usleep(extralargo);
                 exec("gpio -g write 4 1");
         }
         if ($state == "off"){
-                exec("gpio -g write 4 0");
+                exec("gpio -g write 4 1");
         }
         
 ?>
--- script.js
+++ script.js
@@ -1,21 +1,142 @@
 window.onload = function(){
-        buttonOn = document.getElementById('submitOn');
-        buttonOn.onClick = relayOn;
+        botonUsuario1 = document.getElementById('usuario1');
+        buttonOn.onClick = usuario1;
         
+        botonUsuario2 = document.getElementById('usuario2');
+        buttonOn.onClick = usuario2;
+
+        botonUsuario3 = document.getElementById('usuario3');
+        buttonOn.onClick = usuario3;
+
+        botonUsuario4 = document.getElementById('usuario4');
+        buttonOn.onClick = usuario4;
+
+        botonUsuario5 = document.getElementById('usuario5');
+        buttonOn.onClick = usuario5;
+
+        botonUsuario6 = document.getElementById('usuario6');
+        buttonOn.onClick = usuario6;
+
+        botonUsuario7 = document.getElementById('usuario7');
+        buttonOn.onClick = usuario7;
+
+        botonUsuario1 = document.getElementById('usuario1');
+        buttonOn.onClick = usuario1;
+
+        botonUsuario8 = document.getElementById('usuario8');
+        buttonOn.onClick = usuario8;
+
+        botonUsuario9 = document.getElementById('usuario9');
+        buttonOn.onClick = usuario9;
+
+        botonUsuario10 = document.getElementById('usuario10');
+        buttonOn.onClick = usuario10;
+
         buttonOff = document.getElementById('submitOff');
         buttonOff.onClick = relayOff;
 }
 
-function relayOn(){
+function usuario1(){
         
         hidden = document.getElementById("total");
-        hidden.value = "on";
+        hidden.value = "usuario1";
         
         form = document.getElementById("relay");
         form.method = "GET";
         form.action = "remote_relay.php";
         form.submit();
 }
+function usuario2(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario2";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario3(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario3";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario4(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario4()";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario5(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario5";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario6(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario6";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario7(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario7";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario8(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario8";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario9(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario9";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+function usuario10(){
+        
+        hidden = document.getElementById("total");
+        hidden.value = "usuario10";
+        
+        form = document.getElementById("relay");
+        form.method = "GET";
+        form.action = "remote_relay.php";
+        form.submit();
+}
+
 
 function relayOff(){
         
